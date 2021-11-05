e8fb4dbbe1a494ce31d9f966ec1d83d809d3e6d7
IYVVVVV <1695275378@qq.com>
2021-11-05 16:40:29 +0800

    add session time constraint

--- schema.sql
+++ schema.sql
@@ -109,7 +109,13 @@ CREATE TABLE Sessions (
     PRIMARY KEY (room, sfloor, stime, sdate),
     FOREIGN KEY (room, sfloor) REFERENCES Meeting_Rooms (room, mfloor) ON DELETE CASCADE,
     FOREIGN KEY (booker_id) REFERENCES Bookers (eid) ON DELETE CASCADE,
-    FOREIGN KEY (manager_id) REFERENCES Managers (eid) ON DELETE CASCADE
+    FOREIGN KEY (manager_id) REFERENCES Managers (eid) ON DELETE CASCADE,
+    -- session should be hourly
+    CONSTRAINT hourly_session_time CHECK (
+        stime in ('00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00',
+                  '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', 
+                  '13:00', '14:00', '15:00', '16:00', '17:00', '18:00',
+                  '19:00', '20:00', '21:00', '22:00', '23:00', '24:00', ))
 );
 
 -- join
