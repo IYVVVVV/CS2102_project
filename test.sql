2510b646102198631dab9917a257dbcb81eeca2a
IYVVVVV <1695275378@qq.com>
2021-11-05 17:16:33 +0800

    add test.sql

---
+++ test.sql
@@ -0,0 +1,7 @@
+--remove_employee
+--invalid: employee that is booker of some approved meetings
+select "remove_employee"(19, '2021-11-02');
+--invalid: employee that joins of some approved meetings
+select "remove_employee"(9, '2021-11-02');
+--valid: employee who is a booker, remove booked session all related joins
+select "remove_employee"(20, '2021-11-02');
